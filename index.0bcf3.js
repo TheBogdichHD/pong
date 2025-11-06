02fa3ce166adcb45ac40270516ebc9ceb5a316b6
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2025-11-06 22:22:12 +0300

    deploy: update build

--- index.0bcf3.js
+++ index.0bcf3.js
@@ -1,4 +1,4 @@
-System.register(["./application.8ec99.js"], function (_export, _context) {
+System.register(["./application.bed0c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
