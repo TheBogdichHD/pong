026053d273fdc2fd45eba850abe8b51232dc0470
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2025-11-16 23:47:46 +0300

    deploy: update build

--- application.0d31f.js
+++ application.0d31f.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.0792d.json';
+          this.settingsPath = 'src/settings.65d1d.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- index.00234.js
+++ index.00234.js
@@ -1,4 +1,4 @@
-System.register(["./application.bed0c.js"], function (_export, _context) {
+System.register(["./application.0d31f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
