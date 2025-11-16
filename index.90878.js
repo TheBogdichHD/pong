8824305b8f9fe3a8615fc9445ea2ab4be5318913
TheBogdichHD <89939588+TheBogdichHD@users.noreply.github.com>
2025-11-17 00:02:08 +0300

    deploy: fix

--- index.90878.js
+++ index.90878.js
@@ -1,4 +1,4 @@
-System.register(["./application.0d31f.js"], function (_export, _context) {
+System.register(["./application.c9bfb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
